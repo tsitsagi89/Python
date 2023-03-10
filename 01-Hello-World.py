54d01cd5ea86ce605d845a7a0e0d87e8a5f39296
tsitsagi89 <99443438+tsitsagi89@users.noreply.github.com>
2023-03-10 12:53:14 +0400

    Update 01-Hello-World.py

--- 01-Hello-World.py
+++ 01-Hello-World.py
@@ -1,5 +1,28 @@
-#Ask users about their name
+#Exercise 1. Version 1.
+
+#make a variable and Ask users about their name
 name = input("What is your name? ").strip()
 #Concatenate text and variable
 greeting = ("Hello, " + name.title() + ", nice to meet you!")
 print(greeting)
+
+
+#Exercise 1. Version 2.
+print("Hello, " + input("what is your name? ").strip().title() + "," + " nice to meet you!")
+
+
+#Exercise 1. Version 3.
+
+#make a variable and Ask users about their name.
+
+name = input("What is your name? ").strip()
+
+#make conditionals, which responds to people with different names with different greetings.
+if name == "Goga":
+    print("Hello, " + name.title() + ", nice to meet you!")
+elif name == "ოთო":
+    print("გამარჯობა, " + name.title() + ", სასიამოვნოა თქვენი გაცნობა!")
+elif name == "Giorgi":
+    print("Hi, " + name.title() + ", nice to meet you!")
+else:
+    print("Hey, " + name.title() + ", nice to meet you!")
