bcbeeb861b4f69e7533f03bbfd8f05f5b06be6b4
tsitsagi89 <99443438+tsitsagi89@users.noreply.github.com>
2023-03-10 12:22:45 +0400

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,5 @@
+#Ask users about their name
+name = input("What is your name? ").strip()
+#Concatenate text and variable
+greeting = ("Hello, " + name.title() + ", nice to meet you!")
+print(greeting)
