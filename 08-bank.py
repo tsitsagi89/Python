f207fd72a99b39f45e253629f967f1363572a948
tsitsagi89 <99443438+tsitsagi89@users.noreply.github.com>
2023-03-10 18:59:20 +0400

    Create 08-bank.py

---
+++ 08-bank.py
@@ -0,0 +1,11 @@
+#Exercise: Home Federal Savings Bank
+#Create greeting ignored any leading whitespace in the user’s greeting, and treat the user’s greeting case-insensitively.
+greeting = input("Greeting: ").strip().lower()
+
+#conditionals by greetings
+if greeting == "hello":
+    print("$0")
+elif greeting[0] == "h":
+    print("$20")
+else:
+    print("$100")
