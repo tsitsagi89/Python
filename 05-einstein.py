e4a6fdce046c946cd19cebeaa36030e033865428
tsitsagi89 <99443438+tsitsagi89@users.noreply.github.com>
2023-03-10 15:50:07 +0400

    Update 05-einstein.py

--- 05-einstein.py
+++ 05-einstein.py
@@ -1,10 +1,10 @@
 #E = mc2
 
-#create mass
+#create mass and speed of light
 m = int(input("m: "))
-
+c = 300000000
 #calculate energy
-E = m * 300000000**2
+E = m * c**2
 
 #print result
 print("E: " + str(E))
