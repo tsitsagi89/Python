a0c6e29d72dd5209402d586caff870f04c224978
tsitsagi89 <99443438+tsitsagi89@users.noreply.github.com>
2023-03-17 00:47:38 +0400

    Update 14-coke.py

--- 14-coke.py
+++ 14-coke.py
@@ -2,7 +2,10 @@
 i = 0
 while i < 50:
     coin = int(input("insert coin: "))
-    i = i + coin
+    if coin == 10 or coin == 5 or coin == 25:
+         i = i + coin
+    else:
+        i = i + 0
     if i < 50:
         print("Amount Due: ", 50 - i)
     else:
