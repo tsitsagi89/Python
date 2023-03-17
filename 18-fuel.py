ee766053af947e6b5ade7f3c41df331447119bff
tsitsagi89 <99443438+tsitsagi89@users.noreply.github.com>
2023-03-18 01:36:22 +0400

    Create 18-fuel.py

---
+++ 18-fuel.py
@@ -0,0 +1,20 @@
+while True:
+    try:
+        fraction = float(eval(input("Fraction: ")))
+        if fraction > 1:
+            continue
+        elif fraction <= 0.01:
+            print("E")
+            break
+        elif 0.99 <= fraction <= 1:
+            print("F")
+            break
+        else:
+            print("{:.0%}".format(fraction))
+            break
+    except ValueError:
+        pass
+    except ZeroDivisionError:
+        pass
+    except NameError:
+        pass
