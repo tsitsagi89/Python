7497f10c25a6ae7a2c7a6e80a00211676417ee98
tsitsagi89 <99443438+tsitsagi89@users.noreply.github.com>
2023-04-02 00:45:54 +0400

    Update 32-lines.py

--- 32-lines.py
+++ 32-lines.py
@@ -1,8 +1,12 @@
 import sys
 
-with open(f"{sys.argv[1]}", "r") as file:
-    count_lines = 0
-    for line in file:
-        if line != "\n" and not line.startswith("#"):
-            count_lines += 1
+try:
+    with open(f"{sys.argv[1]}", "r") as file:
+        count_lines = 0
+        for line in file:
+            if line != "\n" and not line.startswith("#"):
+                count_lines += 1
+except FileNotFoundError:
+    sys.exit("File Not Found")
+
 print(count_lines)
